a1ea381881b80d27fcf20a058f627669d4f61448
ted <theo_drz@live.com>
2024-04-29 23:58:10 -0400

    created Res class, implemented file based router, still cant get favicon working

--- index.ts
+++ index.ts
@@ -11,12 +11,12 @@
 // NOTES:
 // [ DONE ] All lines in head should end in \r\n, but could end with just \n
 // [ DONE ] All lines in head can have an arbitrary number of tabs/spaces between them
-//  - This request line is still valid:
-//      'GET       /myPath       HTTP/1.0'
-//  - This header is also still valid:
-//      'cookie:      cookieName=cookieValue'
-// If a header line starts with a tab or a space, then it belongs to a previous header key
-// Header names should NOT be case-sensitive, we'll probably make them all lowercase
+//  - [ DONE ] This request line is still valid:
+//    [ DONE ]   'GET       /myPath       HTTP/1.0'
+//  - [ DONE ] This header is also still valid:
+//    [ DONE ]   'cookie:      cookieName=cookieValue'
+// [ DONE ] If a header line starts with a tab or a space, then it belongs to a previous header key
+// [ DONE ] Header names should NOT be case-sensitive, we'll probably make them all lowercase
 // If there is a body in either a request or response, includes the following headers:
 //  - content-type: MIME type, i.e. 'application/json' or 'text/html'
 //  - content-length: The length of the body content in bytes
@@ -55,60 +55,108 @@ class Req {
   }
 }
 
+class Res {
+  httpVersion: string;
+  status: number;
+  reason: string;
+  filePath?: string;
+  headers: StrObj;
+
+  constructor(req: Req) {
+    const match = router.match(req.path)
+    this.httpVersion = 'HTTP/1.0';
+    this.status = match ? 200 : 404;
+    this.reason = match ? 'OK' : 'Not Found';
+    this.filePath = match?.filePath
+    console.log(req.path)
+    this.headers = {
+      'content-type': match ? Bun.file(match.filePath).type : 'text/plain',
+      // 'content-length': match ? Bun.file(match.filePath).size.toString() : '0',
+    }
+  }
+
+  getHeaders() {
+    return Object.keys(this.headers).reduce((headers, key) => {
+      return headers + `${key}: ${this.headers[key]}\r\n`
+    }, '')
+  }
+
+  async getText() {
+    const body = this.filePath ? await Bun.file(this.filePath).text() : '';
+    // if (this.filePath && this.filePath.match(/\.ico$/)) {
+    //   const arrBuf = await Bun.file(this.filePath).arrayBuffer()
+    //   console.log(arrBuf)
+    //   const base64Enc = Buffer.from(arrBuf).toString('base64')
+    //   console.log(base64Enc)
+    //   const body = base64Enc
+    //   return `HTTP/1.0 ${this.status} ${this.reason}\r\n${this.getHeaders()}\r\n${body}`
+    // }
+    return `HTTP/1.0 ${this.status} ${this.reason}\r\n${this.getHeaders()}\r\n${body}`
+  }
+}
+
+const router = new Bun.FileSystemRouter({
+  dir: './src',
+  style: 'nextjs',
+  fileExtensions: ['.js', '.css', '.ico', '.html'],
+  origin: 'localhost:3000'
+});
+console.log(router)
+
 Bun.listen({
   hostname: 'localhost',
   port: 3000,
   socket: {
     async data(socket, data) {
-      const req = new Req(data.toString());
-      console.log(req);
-
-      // Get file from current directory based on path
-      const resBody = `
-        <!doctype html>
-        <html>
-          <body>
-            <h1>Hello World!</h1>
-          </body>
-        </html>
-      `
-
-      socket.end(`HTTP/1.1 200 OK\r\n\r\n${resBody}`)
+      // const req = new Req(data.toString());
+      // socket.end(await (new Res(req)).getText())
+      socket.end(
+        await new Res(
+          new Req(data.toString())
+        ).getText()
+      )
     },
   }
 })
 
-// type Req = {
-//   method: string,
-//   path: string,
-//   httpVersion: string,
-//   headers: { [key: string]: string }
-//   body?: string;
+// type ResProps = [number, string, string | undefined]
+// OLD WORKING RES CLASS
+// body?: string;
+// constructor(status: number, reason: string, body?: string) {
+//   this.httpVersion = 'HTTP/1.0';
+//   this.status = status;
+//   this.reason = reason;
+//   this.body = body;
+// }
+// getText() {
+//   return `${this.httpVersion} ${this.status} ${this.reason}\r\n\r\n${this.body || ''}`
 // }
 
-// const formatHeader: { [key: string]: (str: string) => any } = {
-//   cookie: (str) => str.split('; ').reduce((cookies, rawCookie) => {
-//     const [ name, val ] = rawCookie.split('=')
-//     cookies[name] = val;
-//     return cookies
-//   }, {} as { [key: string]: string })
+// fileExtensions: Object.keys(getDirFromSuffex),
+
+// const getDirFromSuffex: { [key: string]: string } = {
+//   '.js': '/scripts',
+//   '.css': '/styles',
+//   '.ico': '/favicon',
+//   '.html': '/pages',
 // }
 
-// function getReqObj(data: string) {
-//   return data.split('\r\n')
-//     .filter(line => line) // An empty line indicates start of body, in both req and res
-//     .reduce((req, line) => {
-//       if (line.includes(': ')) {
-//         const [ key, val ] = line.split(': ')
-//         req.headers[key] = val;
-//         // req.headers[key] = formatHeader[key.toLowerCase()] ? formatHeader[key.toLowerCase()](val) : val;
-//       } else {
-//         const [ method, path, httpVersion ] = line.split(' ')
-//         req = { ...req, method, path, httpVersion }
-//       }
-// 
-//       return req
-//     }, { headers: {} } as Req)
-// }
+// OLD WORKING, restore /src using notes/old-src
+// const suffex = req.path.match(/\.\w+/)?.[0];
+// const dir = getDirFromSuffex[suffex || '.html'];
+// const file = req.path === '/' ? '/index' : req.path;
+// const fileType = suffex ? '' : '.html';
 
+// console.log(req.path, '->', `./src${dir}${file}${fileType}`);
 
+// const resBody = Bun.file(`./src${dir}${file}${fileType}`);
+
+// const match = router.match(req.path)
+// console.log(req.path, 'ROUTER MATCH', match)
+// const resBody = Bun.file(match?.filePath || 'fakeFile')
+// const resProps: ResProps = (
+//   await resBody.exists() ?
+//     [200, 'OK', await resBody.text()] :
+//     [404, 'Not Found', undefined]
+// );
+// socket.end(new Res(...resProps).getText());
